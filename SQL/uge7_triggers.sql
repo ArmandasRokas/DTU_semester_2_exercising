2015e4784888c3abdd2dea0a87684c6a984934be
Armandas <armandas.rokas@gmail.com>
2019-03-27 14:56:09 +0100

    added triggers exercise

--- SQL/uge7_triggers.sql
+++ SQL/uge7_triggers.sql
@@ -1,20 +1,51 @@
 USE s185144;
+/*******************
+Arrange
+*********************/
+CREATE TABLE document_modified (
+document_id int Primary key,
+last_modifier text
+);
 
+CREATE TABLE document_modified_log (
+action text,
+document_id int,
+last_modifier text,
+changedat DATETIME,
+foreign key (document_id) references document_modified(document_id)
+);
+
+INSERT INTO document_modified values (1, "John");
+
+/**************************
+Act
+***************************/
 DELIMITER $$
-CREATE FUNCTION get_dept_budget_more_than_avarage()
-	returns int
-    BEGIN
-		declare myinteger int default 1;
-		return myinteger;
-	END
-DELIMITER;
-    
+CREATE TRIGGER last_modifiers AFTER UPDATE ON document_modified
+	FOR EACH ROW
+	BEGIN
+		INSERT INTO document_modified_log
+        SET action = 'update',
+        document_id = OLD.document_id,
+        last_modifier = OLD.last_modifier,
+        changedat = NOW();
+	END$$
+DELIMITER ;
 
+/*********************
+Assert
+*********************/
+SHOW TRIGGERS;
 
+UPDATE document_modified 
+SET last_modifier = 'Sun'
+WHERE document_id = 1;
+SELECT * FROM document_modified;
+SELECT * FROM document_modified_log;
 
+/*
+Reference : http://www.mysqltutorial.org/create-the-first-trigger-in-mysql.aspx
+*/
 
-SELECT * FROM department WHERE budget > (SELECT AVG(budget) FROM department) ;
-
-SELECT * FROM department;
 
 
